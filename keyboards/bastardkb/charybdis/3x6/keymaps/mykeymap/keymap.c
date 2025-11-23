bc24443f37f9350382470ed1144d3b5addbd6920
RioHonda <rio523-ith5@keio.jp>
2025-11-23 17:16:59 +0900

    DYNAMIC_KEYMAP_LAYER_COUNTを6に変更し、BASEQWERTYレイヤーを追加

--- keyboards/bastardkb/charybdis/3x6/keymaps/mykeymap/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/mykeymap/keymap.c
@@ -22,7 +22,8 @@ enum charybdis_keymap_layers {
     LAYER_LOWER,
     LAYER_RAISE,
     LAYER_POINTER,
-    LAYER_ADJUST, // 新しいレイヤーを追加
+    LAYER_ADJUST, 
+    LAYER_BASEQWERTY
 };
 
 /** \brief Automatically enable sniping-mode on the pointer layer. */
@@ -108,6 +109,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   XXXXXXX, XXXXXXX, _______,    _______, XXXXXXX
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
+
+  [LAYER_BASEQWERTY] = LAYOUT(
+    // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
+            KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC,
+    // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+           KC_BSPC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_RSFT,
+    // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+           KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, 
+    // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
+                                       KC_SPC, KC_LCTL,   LOWER,      RAISE,  KC_ENT
+    //                            ╰───────────────────────────╯ ╰──────────────────╯
+    ),
 };
 // clang-format on
 
